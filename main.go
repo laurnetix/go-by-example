21fd53fe922ca872b9ac6cb3aa5fce6ef7253d19
Lauren <laurnetix@icloud.com>
2025-08-07 22:47:23 -0400

    Go by example third example

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
+/* Go By Example */
+
+// Comments in go are in C-Style
+// and C++ style
+
+// Import modules
 import (
 	"gobyexample/hello"
 	"gobyexample/values"
+	"gobyexample/variables"
 )
 
+// Only one main function per project
 func main() {
 	hello.Hello()
 	values.Values()
+	variables.Variables()
 }
